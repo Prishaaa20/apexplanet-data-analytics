10f76ea76bff5fb06d570b7106794084d65c86a0
Prisha Gautam <prisha.25scs1003004085@iilm.edu>
2026-07-06 20:13:12 +0530

    Remove test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import plotly.express as px
-import sqlalchemy
-
-print("All libraries are working!")
